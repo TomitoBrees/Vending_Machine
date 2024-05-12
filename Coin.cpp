93a7783b50766c8fee4436d116a12a7d7ff6e8fe
Victor Rigot <s4071512@student.rmit.edu.au>
2024-05-12 20:01:23 +1000

    add the function to give back change

--- Coin.cpp
+++ Coin.cpp
@@ -1,3 +1,56 @@
 #include "Coin.h"
- 
- // implement functions for managing coins; this may depend on your design.
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+
+std::vector<std::vector<int>> giveChange(int cents, std::vector<std::vector<int>> availableCoins)
+{
+    int i = 0;
+    std::vector<std::vector<int>> finalChange = {{5000, 0}, {2000, 0}, {1000, 0}, {500,0}, {200, 0},
+                                                 {100, 0}, {50, 0}, {20, 0}, {10, 0}, {5, 0}};
+    while(cents > 0)
+    {
+        if (i == 10)
+        {
+            finalChange[0][0] = -1;
+            return finalChange;
+        }
+
+        if(availableCoins[i][1] == 0)
+        {
+            i += 1;
+        }
+
+        else
+        {
+            if (cents - availableCoins[i][0] >= 0)
+            {
+                availableCoins[i][1] -= 1;
+                cents -= availableCoins[i][0];
+                finalChange[i][1] += 1;
+            }
+            else
+            {
+                i += 1;
+            }
+        }
+    }
+    return finalChange;
+}
+
+ int main()
+ {
+     int cents = 12500;
+     std::vector<std::vector<int>> availableCoins = {{5000, 1}, {2000, 4}, {1000, 4}, {500,5}, {200, 6},
+                                                     {100, 3}, {50, 0}, {20, 0}, {10, 0}, {5, 0}};
+     std::vector<std::vector<int>> finalChange = giveChange(cents, availableCoins);
+    std::cout << finalChange[0][1] << finalChange[1][1] << finalChange[2][1] << finalChange[3][1] << std::endl;
+    return EXIT_SUCCESS;
+ }
+
+
+
+
+
